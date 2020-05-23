db8b6bbbb46138c2f3dd8f6e8a8691440dd9584f
kayo <rayner.kayo.tec@gmail.om>
2020-05-23 14:17:10 -0300

    Commit inicial

---
+++ src/main/java/br/com/rayner/ms/fornecedor/MsFornecedorApplication.java
@@ -0,0 +1,13 @@
+package br.com.rayner.ms.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFornecedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFornecedorApplication.class, args);
+	}
+
+}
